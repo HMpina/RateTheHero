95392875d4fc8585cbd70c02034d25e24fa46929
HMpina <henrique.marques.pina@gmail.com>
2022-10-29 10:45:58 -0300

    feat: create HeroCard component and Search screen structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { NormalizeStyles } from './shared/NormalizeStyles';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Search } from './screens/Search';
 import { Details } from './screens/Details';
+import { Header } from './common-components/Header/Header';
 export function App() {
 	return (
+		<>
+		<NormalizeStyles/>
 		<BrowserRouter>
+		<Header/>
 			<Switch>
 				<Route path="/detalhes/:id" exact>
 					<Details />
@@ -16,5 +21,6 @@ export function App() {
 				<Route path="*">Página não encontrada</Route>
 			</Switch>
 		</BrowserRouter>
+		</>
 	);
 }
