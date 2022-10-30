76cd90bab7fb45a232615b122ad749f1349699d0
HMpina <henrique.marques.pina@gmail.com>
2022-10-30 11:15:17 -0300

    busca utilizando APIs

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,59 +1,83 @@
-import React from 'react';
-import { Flex, Box } from 'reflexbox';
-import styled from 'styled-components';
-import { Button } from '../common-components/Button/Button';
-import { SearchField } from '../common-components/SearchFields/SearchFields';
-import { HeroCard } from '../components/HeroCard/HeroCard';
-import { Spaces } from '../shared/DesignTokens';
+import React from "react";
+import axios from "axios";
+import { Flex, Box } from "reflexbox";
+import styled from "styled-components";
+import { Button } from "../common-components/Button/Button";
+import { SearchField } from "../common-components/SearchFields/SearchFields";
+import { HeroCard } from "../components/HeroCard/HeroCard";
+import { Spaces } from "../shared/DesignTokens";
 const HeroesGrid = styled(Box)`
-	display: grid;
-	grid-template-columns: 1fr;
-	gap: ${Spaces.ONE_HALF};
-	@media (min-width: 1024px) {
-		grid-template-columns: 1fr 1fr 1fr 1fr;
-		gap: ${Spaces.TWO};
-	}
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: ${Spaces.ONE_HALF};
+  @media (min-width: 1024px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: ${Spaces.TWO};
+  }
 `;
 export function Search() {
-	return (
-		<>
-			<Flex
-				width={['100%', '600px']}
-				mx={[Spaces.None, 'auto']}
-				mt={[Spaces.THREE, Spaces.FIVE]}
-				px={[Spaces.ONE, Spaces.NONE]}
-				mb={[Spaces.TWO, Spaces.FOUR]}
-			>
-				<Box flexGrow="1">
-					<SearchField placeholder="Digite um nome de herói ou heroína" />
-				</Box>
-				<Box ml={Spaces.TWO}>
-					<Button>Buscar</Button>
-				</Box>
-			</Flex>
-			<HeroesGrid
-				px={[Spaces.ONE, Spaces.TWO]}
-				pb={[Spaces.ONE, Spaces.TWO]}
-			>
-				<HeroCard
-					secretIdentity="Terry McGinnis"
-					name="Batman"
-					picture="https://www.superherodb.com/pictures2/portraits/10/100/10441.jpg"
-					universe="DC Comics"
-				/>
-				<HeroCard
-					secretIdentity="Bruce Wayne"
-					name="Batman"
-					picture="https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
-					universe="DC Comics"
-				/>
-				<HeroCard
-					secretIdentity="Dick Grayson"
-					name="Batman II"
-					picture="https://www.superherodb.com/pictures2/portraits/10/100/1496.jpg"
-					universe="DC Comics"
-				/>
-			</HeroesGrid>
-		</>
-	);
+  async function searchHero(heroName) {
+    const { data } = await axios.get(`/search/${heroName}`, {
+      baseURL: `${process.env.REACT_APP_SUPER_HERO_API_BASE_URL}/${process.env.REACT_APP_SUPER_HERO_API_KEY}`,
+    });
+    return data.results || [];
+  }
+
+  const [heroes, setHeroes] = React.useState([]);
+  const [search, setSearch] = React.useState({
+    value: null,
+    doSearch: false,
+  });
+  
+
+  React.useEffect(() => {
+    if (search.doSearch) {
+      searchHero(search.value).then((heroes) => {
+        setHeroes(heroes);
+        setSearch((prevValeu) => ({ ...prevValeu, doSearch: false }));
+      });
+    }
+  }, [search]);
+
+  function handleUpdateSearchValue({ target: { value } }) {
+    setSearch((prevValue) => ({ ...prevValue, value }));
+  }
+  function handleSearch() {
+    setSearch((prevValue) => ({ ...prevValue, doSearch: true }));
+  }
+
+  return (
+    <>
+      <Flex
+        width={["100%", "600px"]}
+        mx={[Spaces.None, "auto"]}
+        mt={[Spaces.THREE, Spaces.FIVE]}
+        px={[Spaces.ONE, Spaces.NONE]}
+        mb={[Spaces.TWO, Spaces.FOUR]}
+      >
+        <Box flexGrow="1">
+          <SearchField
+            placeholder="Digite um nome de herói ou heroína"
+            onKeyUp={handleUpdateSearchValue}
+          />
+        </Box>
+        <Box ml={Spaces.TWO}>
+          <Button onClick={handleSearch}>Buscar</Button>
+        </Box>
+      </Flex>
+
+      <HeroesGrid px={[Spaces.ONE, Spaces.TWO]} pb={[Spaces.ONE, Spaces.TWO]}>
+        {heroes.map((hero) => (
+          <HeroCard
+            key={hero.id}
+            id={hero.id}
+            secretIdentity={hero.biography["full-name"]}
+            name={hero.name}
+            picture={hero.image.url}
+            universe={hero.biography.publisher}
+          />
+        ))}
+      </HeroesGrid>
+    </>
+  );
 }
